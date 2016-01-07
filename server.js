d1dd9e05c39bcee9f2cec6787100b0a4026e9fd3
Robert Cornell <robert.cornell78@gmai.com>
2016-01-06 23:03:08 -0700

    tutorial complete

--- server.js
+++ server.js
@@ -19,12 +19,29 @@ app.get('/', (request, response) => {
   response.render('index');
 });
 
+app.get('/pizzas/:id', (request, response) => {
+  var pizza = app.locals.pizzas[request.params.id];
+
+  response.render('pizza', { pizza: pizza });
+});
+
+
 app.post('/pizzas', (request, response) => {
   var id = generateId();
 
   app.locals.pizzas[id] = request.body;
 
   response.sendStatus(201);
+});
+
+app.post('/pizzas', (request, response) => {
+  if (!request.body.pizza) { return response.sendStatus(400); }
+
+  var id = generateId();
+
+  app.locals.pizzas[id] = request.body.pizza;
+
+  response.redirect('/pizzas/' + id);
 });
 
 if (!module.parent) {
